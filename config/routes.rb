442432dcabe28301f462f4f34fe0164fbce24b11
sleepy <seoweefeng@gmail.com>
2017-12-13 20:42:45 +0800

    added reservation form

--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,18 @@ Rails.application.routes.draw do
     resource :password,
       controller: "clearance/passwords",
       only: [:create, :edit, :update]
+    resources :reservations, controller: "reservations"
   end
-  resources :listings
+  resources :listings,controller: "listings" do 
+    resources :reservations, controller: "reservations"
+  end 
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	
+
 	get "/auth/:provider/callback" => "sessions#create_from_omniauth"
 
-	root 'clearance/users#new'
+	root 'listings#index'
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171213113020) do
+ActiveRecord::Schema.define(version: 20171213113021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,17 @@ ActiveRecord::Schema.define(version: 20171213113020) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.bigint "user_id"
+    t.bigint "listing_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["listing_id"], name: "index_reservations_on_listing_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -49,4 +60,6 @@ ActiveRecord::Schema.define(version: 20171213113020) do
   end
 
   add_foreign_key "authentications", "users"
+  add_foreign_key "reservations", "listings"
+  add_foreign_key "reservations", "users"
 end
