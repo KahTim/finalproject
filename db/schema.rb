94064f040adc08411888adb12545998c6beb042e
stalfire <engchienhowe@hotmail.com>
2017-12-14 11:31:59 +0800

    seed listing done

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 20171213113020) do
     t.string "title"
     t.string "address"
     t.string "city"
-    t.string "type"
+    t.string "category"
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
