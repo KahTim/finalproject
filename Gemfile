c1a71bd1c20bcca08cb530d5721548d7ca8c3e42
KahTim <kahtim.tang@gmail.com>
2017-12-14 13:22:58 +0800

    done w user sign in w fb sign up and edit

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ gem 'figaro'
 gem 'omniauth'
 gem 'omniauth-facebook'
 gem 'jquery-rails'
+gem 'mini_magick'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,14 @@ Rails.application.routes.draw do
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "clearance/sessions", only: [:create]
 
-  resources :users, controller: "clearance/users", only: [:create] do
+  resources :users, controller: "users", only: [:create, :edit, :update] do
     resource :password,
       controller: "clearance/passwords",
       only: [:create, :edit, :update]
   end
+
+  resources :users, controller: "users", only: [:create, :edit, :update]
+
   resources :listings
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
@@ -17,5 +20,5 @@ Rails.application.routes.draw do
 	
 	get "/auth/:provider/callback" => "sessions#create_from_omniauth"
 
-	root 'clearance/users#new'
+	root 'home#index'
 end
