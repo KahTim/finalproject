e06105e10a0e672744231d98b8f2090ccf5e02ae
stalfire <engchienhowe@hotmail.com>
2017-12-14 14:23:56 +0800

    search engine

--- config/routes.rb
+++ config/routes.rb
@@ -8,16 +8,18 @@ Rails.application.routes.draw do
     resource :password,
       controller: "clearance/passwords",
       only: [:create, :edit, :update]
+    resources :reservations, controller: "reservations"
   end
 
-  resources :users, controller: "users", only: [:create, :edit, :update]
+  resources :listings,controller: "listings" do 
+    resources :reservations, controller: "reservations"
+  end 
 
-  resources :listings
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	
+
 	get "/auth/:provider/callback" => "sessions#create_from_omniauth"
 
 	root 'home#index'
---
+++ db/migrate/20171213113021_create_reservations.rb
@@ -0,0 +1,11 @@
+class CreateReservations < ActiveRecord::Migration[5.1]
+  def change
+    create_table :reservations do |t|
+      t.datetime :start_time
+      t.datetime :end_time 
+      t.references :user, foreign_key: true
+      t.references :listing, foreign_key: true 
+      t.timestamps
+    end
+  end
+end
