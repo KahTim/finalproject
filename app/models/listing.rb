c6bae191585a3c44a8647e96847ee30b79226a54
stalfire <engchienhowe@hotmail.com>
2017-12-20 14:56:36 +0800

    reservation delete

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,7 +2,7 @@ class Listing < ApplicationRecord
 	mount_uploaders :avatar, AvatarUploader
 	serialize :avatars, JSON
 	belongs_to :user
-	has_many :reservations
+	has_many :reservations, dependent: :destroy
 	
 	scope :city, -> (city) { where city: city}
 	scope :category, -> (category) { where category: category}
